1b4f46273c45b014c634ce388381a78b8c49f8de
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-10-05 14:27:03 -0600

    Ajuste botón agrega tarea

---
+++ src/components/inputs/ButtonAddTask.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: "ButtonAddTask",
+    props: {
+        handleAddTasks: Function,
+    },
+    methods: {
+        handleAddTasksClick() {
+            this.handleAddTasks();
+        },
+    },
+};
+</script>
+<template>
+    <!-- <button @click="handleAddTasksClick"
+        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-lg">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    </button> -->
+
+    <button @click="handleAddTasksClick" type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-xl p-6 text-center inline-flex items-center mr-2 ">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+            <path
+                d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+        </svg>
+
+    </button>
+</template>
