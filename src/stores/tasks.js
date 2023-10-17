1e75366fa279dc7c7c1655748eb206f52be6e2c3
Jose Juan Regalado <jregaladomoncada@gmail.com>
2023-10-16 22:38:32 -0600

    Integración con local storage

--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -1,82 +1,10 @@
 import { defineStore } from 'pinia';
-
-export const useTasksStore = defineStore('tasks', {
+const STORE_NAME = 'tasks';
+const tasks = localStorage.getItem(STORE_NAME);
+export const useTasksStore = defineStore(STORE_NAME, {
     state: () => ({
         statusTask: 'All Tasks',
-        tasks: [
-            {
-                id: '0404044-4404040404',
-                name: 'Task 1941',
-                status: 'In Progress',
-                level: 'Medium',
-                created: '2023-10-03',
-                activities: [
-                    {
-                        name: 'Activity 1',
-                        status: 'In Progress',
-                        created: '2023-10-01',
-                    },
-                    {
-                        name: 'Activity 2',
-                        status: 'In Progress',
-                        created: '2023-10-03',
-                    },
-                    {
-                        name: 'Activity 3',
-                        status: 'In Progress',
-                        created: '2021-08-01',
-                    },
-                ],
-            },
-            {
-                id: '0404044-4404040404',
-                name: 'Task 1941',
-                status: 'In Progress',
-                level: 'Medium',
-                created: '2023-10-01',
-                activities: [
-                    {
-                        name: 'Activity 1',
-                        status: 'In Progress',
-                        created: '2023-10-01',
-                    },
-                    {
-                        name: 'Activity 2',
-                        status: 'In Progress',
-                        created: '2023-10-03',
-                    },
-                    {
-                        name: 'Activity 3',
-                        status: 'In Progress',
-                        created: '2021-08-01',
-                    },
-                ],
-            },
-            {
-                id: '0404044-AD4P54555',
-                name: 'Task 2',
-                status: 'In Progress',
-                level: 'Medium',
-                created: '2023-10-02',
-                activities: [
-                    {
-                        name: 'Activity 1',
-                        status: 'In Progress',
-                        created: '2021-08-01',
-                    },
-                    {
-                        name: 'Activity 2',
-                        status: 'In Progress',
-                        created: '2021-08-01',
-                    },
-                    {
-                        name: 'Activity 3',
-                        status: 'In Progress',
-                        created: '2021-08-01',
-                    },
-                ]
-            },
-        ]
+        tasks: tasks ? JSON.parse(tasks) : [],
     }),
     getters: {
         getTasks() {
@@ -117,6 +45,7 @@ export const useTasksStore = defineStore('tasks', {
                 ],
             };
             this.tasks.push(task);
+            localStorage.setItem(STORE_NAME, JSON.stringify(this.tasks))
         }
 
     },
